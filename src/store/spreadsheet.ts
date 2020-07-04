f82b707e77325db27c986094013e7327e43620ac
tkzwhr <hi@tkzwhr.jp>
2020-07-04 17:50:30 +0900

    add spreadsheet store.

---
+++ src/store/spreadsheet.ts
@@ -0,0 +1,164 @@
+import { Mutation, Action, VuexModule, Module, getModule } from 'vuex-module-decorators';
+import store from './index';
+import axios from 'axios';
+
+export interface Word {
+  front: string;
+  back: string;
+}
+
+export interface SpreadsheetSheetState {
+  fetching: boolean;
+  fetchingErrors: Array<string>;
+  sheetId: string;
+  sheetName: string;
+  words: Array<Word>;
+}
+
+export interface SpreadsheetState {
+  fetching: boolean;
+  fetchingErrors: Array<string>;
+  url: string | null;
+  sheets: Array<SpreadsheetSheetState>;
+}
+
+@Module({
+  dynamic: true,
+  store,
+  name: "spreadsheet",
+  namespaced: true,
+  preserveState: localStorage.getItem('vuex') !== null
+})
+class Spreadsheet extends VuexModule implements SpreadsheetState {
+  fetching = false;
+  fetchingErrors: Array<string> = [];
+  url: string | null = null;
+  sheets: Array<SpreadsheetSheetState> = [];
+
+  @Mutation
+  START_FETCHING() {
+    this.fetching = false;
+    this.fetchingErrors = [];
+  }
+
+  @Mutation
+  SET_SHEETS(data: { url: string; sheets: Array<SpreadsheetSheetState> }) {
+    this.fetching = false;
+    this.fetchingErrors = [];
+    this.url = data.url;
+    this.sheets = data.sheets;
+  }
+
+  @Mutation
+  SET_SPREADSHEET_ERRORS(errors: Array<string>) {
+    this.fetching = false;
+    this.fetchingErrors = errors;
+  }
+
+  @Mutation
+  DELETE() {
+    this.fetching = false;
+    this.fetchingErrors = [];
+    this.url = null;
+    this.sheets = [];
+  }
+
+  @Mutation
+  START_FETCHING_SHEET(sheetId: string) {
+    const sheet = this.sheets.find(t => t.sheetId === sheetId);
+    if (sheet) {
+      sheet.fetching = true;
+      sheet.fetchingErrors = [];
+    }
+  }
+
+  @Mutation
+  SET_WORDS(data: { sheetId: string; words: Array<Word> }) {
+    const sheet = this.sheets.find(t => t.sheetId === data.sheetId);
+    if (sheet) {
+      sheet.fetching = false;
+      sheet.fetchingErrors = [];
+      sheet.words = data.words;
+    }
+  }
+
+  @Mutation
+  SET_SHEET_ERRORS(data: { sheetId: string; errors: Array<string> }) {
+    const sheet = this.sheets.find(t => t.sheetId === data.sheetId);
+    if (sheet) {
+      sheet.fetching = false;
+      sheet.fetchingErrors = data.errors;
+    }
+  }
+
+  @Action({ rawError: true })
+  async fetch(spreadsheetId: string) {
+    try {
+      // Fetch a worksheet
+      this.START_FETCHING();
+      const url = `https://spreadsheets.google.com/feeds/worksheets/${spreadsheetId}/public/basic?alt=json`;
+      const data = await axios.get(url);
+      const sheets: Array<SpreadsheetSheetState> = data.data.feed.entry.map((t: any) => ({
+        fetching: false,
+        fetchingErrors: [],
+        sheetId: t.id['$t'].split("/").slice(-1)[0],
+        sheetName: t.title['$t'],
+        words: []
+      }));
+      this.SET_SHEETS({
+        url: `https://docs.google.com/spreadsheets/d/${spreadsheetId}/edit`,
+        sheets
+      });
+
+      // Fetch all sheets
+      sheets.map(async t => {
+        try {
+          this.START_FETCHING_SHEET(t.sheetId);
+          const url = `https://spreadsheets.google.com/feeds/list/${spreadsheetId}/${t.sheetId}/public/values?alt=json`;
+          const data = await axios.get(url);
+          const words: Array<Word> = data.data.feed.entry.map((u: any) => ({
+            front: u['gsx$front']['$t'],
+            back: u['gsx$back']['$t']
+          }));
+          if (words.length < 4) {
+            throw { message: "too few words" };
+          }
+          this.SET_WORDS({sheetId: t.sheetId, words: words});
+        } catch (e) {
+          console.log(e);
+          const errors: Array<string> = [];
+          switch (e.message) {
+            case '_data.data.feed.entry is undefined':
+              errors.push('No words are found.');
+              break;
+            case 'u.gsx$front is undefined':
+              errors.push('`Front` is not defined at first row.');
+              break;
+            case 'u.gsx$back is undefined':
+              errors.push('`Back` is not defined at first row.');
+              break;
+            case 'too few words':
+              errors.push('At least 4 rows are required.');
+              break;
+            default:
+              errors.push('Unknown error');
+          }
+          this.SET_SHEET_ERRORS({sheetId: t.sheetId, errors});
+        }
+      });
+    } catch (e) {
+      const errors: Array<string> = [];
+      switch (e.message) {
+        case 'Network Error':
+          errors.push('Cannot access to spreadsheet.');
+          break;
+        default:
+          errors.push('Unknown error');
+      }
+      this.SET_SPREADSHEET_ERRORS(errors);
+    }
+  }
+}
+
+const $spreadsheet = getModule(Spreadsheet, store);
+export default $spreadsheet;
--- src/views/Top.vue
+++ src/views/Top.vue
@@ -8,12 +8,15 @@
         <div class="spreadsheet-url">
           <el-input
             v-model="newSheetUrl"
-            :readonly="sheetUrl !== null"
+            :readonly="spreadsheet.url !== null"
             placeholder="https://docs.google.com/spreadsheets/d/abcdefghijklmnopqrstuvwxyz1234567890/edit#gid=0"
           ></el-input>
         </div>
-        <div v-if="sheetUrl === null">
-          <el-button type="primary" @click="syncSpreadsheet">Sync</el-button>
+        <div v-if="spreadsheet.url === null">
+          <el-tooltip v-if="urlError !== null" class="item" effect="dark" :content="urlError" placement="top">
+            <el-button disabled>Sync</el-button>
+          </el-tooltip>
+          <el-button v-else type="primary" @click="syncSpreadsheet">Sync</el-button>
         </div>
         <div v-else>
           <el-button @click="syncSpreadsheet" class="button-margin-right">Re-Sync</el-button>
@@ -24,55 +27,78 @@
             cancel-button-text='Cancel'
             icon="el-icon-info"
             icon-color="red"
+            @onConfirm="deleteSpreadsheet"
           >
             <el-button slot="reference" type="danger">Delete</el-button>
           </el-popconfirm>
         </div>
       </div>
 
-      <el-table v-if="sheetUrl !== null" :data="tableData" height="50vh">
-        <el-table-column
-          label="Sheet Name"
-          prop="sheetName">
+      <el-table
+        v-if="spreadsheet.url !== null && !dialogVisible"
+        :data="spreadsheet.sheets"
+        height="50vh"
+      >
+        <el-table-column label="Sheet Name">
+          <template slot-scope="scope">
+            <div v-if="scope.row.fetchingErrors.length > 0">
+              <el-tooltip :content="scope.row.fetchingErrors.join('\n')" placement="top">
+                <span>
+                  <i class="el-icon-warning"></i>
+                  <span style="color: darkgrey">
+                    {{scope.row.sheetName}}
+                  </span>
+                </span>
+              </el-tooltip>
+            </div>
+            <div v-else>
+              {{scope.row.sheetName}}
+            </div>
+          </template>
         </el-table-column>
+        <el-table-column
+          label="Number of Words"
+          prop="words.length"
+          align="right"
+        ></el-table-column>
         <el-table-column
           align="right"
           label="Latest Used Date"
-          prop="latestUsedDate">
-        </el-table-column>
+          prop="latestUsedDate"
+        ></el-table-column>
       </el-table>
 
-      <el-dialog title="Synchronizing sheets..." :visible.sync="dialogVisible">
+      <el-dialog title="Synchronizing with Google Spreadsheets..." :visible.sync="dialogVisible">
         <el-alert
-          v-if="loadingMessage"
+          v-if="spreadsheet.fetchingErrors.length > 0"
           type="error"
-          :title="loadingMessage"
+          :title="spreadsheet.fetchingErrors.join('\n')"
           :closable="false"
           show-icon>
         </el-alert>
-        <el-table v-else :data="sheets" v-loading="isLoadingSpreadsheet">
+        <el-table v-else v-loading="spreadsheet.fetching" :data="spreadsheet.sheets" >
           <el-table-column label="Sheet Name" prop="sheetName"></el-table-column>
           <el-table-column align="right">
             <template slot-scope="scope">
               <el-tooltip
-                v-if="scope.row.status.message"
-                :content="scope.row.status.message"
+                v-if="scope.row.fetchingErrors.length > 0"
+                :content="scope.row.fetchingErrors.join('\n')"
                 placement="top"
               >
                 <el-button
                   size="mini"
-                  :type="scope.row.status.type"
-                  :icon="scope.row.status.icon"
-                  :loading="scope.row.status.loading"
+                  :type="sheetStatusType(scope.row)"
+                  :icon="sheetStatusIcon(scope.row)"
+                  :loading="scope.row.fetching"
                   circle
                 ></el-button>
               </el-tooltip>
               <el-button
                 v-else
                 size="mini"
-                :type="scope.row.status.type"
-                :icon="scope.row.status.icon"
-                :loading="scope.row.status.loading"
+                :type="sheetStatusType(scope.row)"
+                :icon="sheetStatusIcon(scope.row)"
+                :loading="scope.row.fetching"
                 circle
               ></el-button>
             </template>
@@ -84,124 +110,76 @@
 </template>
 
 <script lang="ts">
-  import axios from 'axios';
   import dayjs from 'dayjs';
   import relativeTime from 'dayjs/plugin/relativeTime';
-  import { Component, Vue } from 'vue-property-decorator';
-  import $wordNote from '@/store/wordNote';
+  import { Component, Watch, Vue } from 'vue-property-decorator';
+  import $spreadsheet, { SpreadsheetState, SpreadsheetSheetState } from '@/store/spreadsheet';
 
   dayjs.extend(relativeTime);
 
   @Component
   export default class Top extends Vue {
-    private sheetUrl: string | null = 'https://docs.google.com/spreadsheets/d/1iH55w3rJxZu0wBGs0AoAqwUpE_iSo28-QTU8voB7EMY/edit#gid=0';
-    //private sheetUrl: string | null = null;
-    private newSheetUrl = this.sheetUrl;
-
-    private sheets: Array<any> = [];
+    private newSheetUrl: string = this.spreadsheet.url ?? '';
+    private urlError: string | null = null;
     private dialogVisible = false;
-    private isLoadingSpreadsheet = false;
-    private loadingMessage: string | null = null;
 
-    private tableData: Array<any> = [];
+    get spreadsheet(): SpreadsheetState {
+      return {
+        url: $spreadsheet.url,
+        fetching: $spreadsheet.fetching,
+        fetchingErrors: $spreadsheet.fetchingErrors,
+        sheets: $spreadsheet.sheets,
+      };
+    }
 
-    private applyStatus(code: string, message: string | null): any {
-      switch (code) {
-        case 'SUCCESS':
-          return {
-            type: 'success',
-            loading: false,
-            icon: 'el-icon-check',
-            message
-          };
-        case 'FAILED':
-          return {
-            type: 'warning',
-            loading: false,
-            icon: 'el-icon-close',
-            message
-          };
-        default:
-          return {
-            type: 'default',
-            loading: true,
-            icon: null,
-            message
-          };
+    get spreadsheetId(): string | null {
+      const result = this.newSheetUrl.match(/^https:\/\/docs.google.com\/spreadsheets\/d\/([^/]+)\/.*$/);
+      return (result ?? [null, null])[1];
+    }
+
+    @Watch('newSheetUrl')
+    validateUrl() {
+      if (!this.spreadsheetId) {
+        this.urlError = "Specified url is malformed.";
+        return;
+      }
+      this.urlError = null;
+    }
+
+    sheetStatusType(data: SpreadsheetSheetState): string {
+      if (!data.fetching && data.fetchingErrors.length === 0) {
+        return "success";
+      } else if (!data.fetching && data.fetchingErrors.length > 0) {
+        return "warning";
+      } else {
+        return "default";
+      }
+    }
+
+    sheetStatusIcon(data: SpreadsheetSheetState): string {
+      if (!data.fetching && data.fetchingErrors.length === 0) {
+        return "el-icon-check";
+      } else if (!data.fetching && data.fetchingErrors.length > 0) {
+        return "el-icon-close";
+      } else {
+        return "";
       }
     }
 
     syncSpreadsheet() {
-      this.dialogVisible = true;
-      this.loadingMessage = null;
-      this.isLoadingSpreadsheet = true;
-      setTimeout(() => {
-        this.isLoadingSpreadsheet = false;
-        this.loadingMessage = 'invalid';
-        this.sheets = [
-          {
-            sheetId: "a1",
-            sheetName: "シート１",
-            status: this.applyStatus('LOADING', null)
-          },
-          {
-            sheetId: "a2",
-            sheetName: "シート２",
-            status: this.applyStatus('LOADING', null)
-          },
-          {
-            sheetId: "a3",
-            sheetName: "シート３",
-            status: this.applyStatus('LOADING', null)
-          }
-        ];
-        setTimeout(() => {
-          this.sheets.find(t => t.sheetId === 'a1').status = this.applyStatus('SUCCESS', null);
-        }, 1000);
-        setTimeout(() => {
-          this.sheets.find(t => t.sheetId === 'a2').status = this.applyStatus('FAILED', 'Invalid format.');
-        }, 1500);
-        setTimeout(() => {
-          this.sheets.find(t => t.sheetId === 'a3').status = this.applyStatus('SUCCESS', null);
-        }, 800);
-      }, 3000);
+      if (this.spreadsheetId) {
+        this.dialogVisible = true;
+        $spreadsheet.fetch(this.spreadsheetId);
+      }
     }
 
-    // async onSubmit() {
-    //   try {
-    //     await axios.get(`https://spreadsheets.google.com/feeds/list/${this.form.spreadsheetId}/${this.form.spreadsheetNumber.toString()}/public/values?alt=json-in-script`);
-    //     await $wordNote
-    //       .download({
-    //         sheetId: this.form.spreadsheetId,
-    //         sheetNumber: this.form.spreadsheetNumber.toString()
-    //       })
-    //     await this.$router.push({
-    //       name: 'Quiz',
-    //       params: {
-    //         holderId: `${this.form.spreadsheetId}-${this.form.spreadsheetNumber}`
-    //       }
-    //     } as any)
-    //   } catch (err) {
-    //     if (err.message === 'Request failed with status code 400') {
-    //       console.log((this as any).$notify);
-    //       (this as any).$notify.error({
-    //         title: 'Error',
-    //         message: 'Spreadsheetの読み込みに失敗しました。'
-    //       });
-    //     } else if (err.message === 't.gsx$front is undefined') {
-    //       console.log((this as any).$notify);
-    //       (this as any).$notify.error({
-    //         title: 'Error',
-    //         message: '1行目にFrontがありません。'
-    //       });
-    //     } else if (err.message === 't.gsx$back is undefined') {
-    //       (this as any).$notify.error({
-    //         title: 'Error',
-    //         message: '1行目にBackがありません。'
-    //       });
-    //     }
-    //   }
-    // }
+    deleteSpreadsheet() {
+      $spreadsheet.DELETE();
+    }
+
+    mounted() {
+      this.validateUrl();
+    }
   }
 </script>
 
